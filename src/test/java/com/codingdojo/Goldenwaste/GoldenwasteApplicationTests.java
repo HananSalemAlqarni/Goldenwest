1aae5c255c7a5614cf8fae414f6c704b162de14e
HananSalemAlqarni <hsh7al@gmail.com>
2022-09-12 16:31:09 +0300

    new

---
+++ src/test/java/com/codingdojo/Goldenwaste/GoldenwasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.Goldenwaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenwasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
